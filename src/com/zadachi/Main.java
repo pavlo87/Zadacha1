0fff33b35febf363519c0093ef8ba386a3370595
pavlo87 <zagorbenskijp@gmail.com>
2018-10-21 10:20:03 +0300

    Initial commit

---
+++ src/com/zadachi/Main.java
@@ -0,0 +1,13 @@
+package com.zadachi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1;i<=512; i=i*2)
+
+        {
+            System.out.print(i+" ");
+
+        }
+    }
+}
